ab1ec40055f2a45e60650399df59d72c440b5d84
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2020-02-29 13:16:42 +0300

    Update problems.sql

--- problems.sql
+++ problems.sql
@@ -5,10 +5,10 @@ ORDER BY a.joined_at DESC
 /*2*/
 SELECT result.community_id, c.name, p.name, count(result.user_id)  FROM users u, permissions p, communities c,
 (
-  SELECT community_members.id, community_members.community_id, community_members.user_id, community_member_permissions.permission_id  FROM community_members 
-  JOIN community_member_permissions ON community_members.user_id=community_member_permissions.member_id
+    SELECT  community_members.community_id, community_members.user_id, community_member_permissions.permission_id  FROM community_members 
+    JOIN community_member_permissions ON community_members.user_id=community_member_permissions.member_id
 )AS result
 WHERE result.user_id=u.id AND result.permission_id=p.id AND result.community_id=c.id
-GROUP BY p.name,c.name,result.community_id
-ORDER BY result.community_id DESC
+GROUP BY p.name,c.name,result.community_id HAVING result.community_id>4
+ORDER BY result.community_id DESC, p.name ASC
 LIMIT 100
