b0603654e5ab1e175f6166de81044a02be2ed876
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2020-02-29 10:12:59 +0300

    Update problems.sql

--- problems.sql
+++ problems.sql
@@ -3,7 +3,15 @@ SELECT  a.joined_at, b.name, c.name FROM community_members a, users b, communiti
 WHERE a.joined_at > '2013-01-01 00:00:00' AND a.user_id=b.id AND a.community_id=c.id 
 ORDER BY a.joined_at DESC
 /*2*/
-SELECT cmp.id, p.name, count(u.name) FROM users u, communities c, permissions p, community_members cm, community_member_permissions cmp
-WHERE cmp.member_id=u.id AND cmp.permission_id=p.id 
-ORDER BY p.name DESC
-LIMIT 100
+SELECT c.id, c.name, p.name, COUNT(result_cmp.member_id)  FROM communities c, permissions p, 
+(
+  SELECT cm.community_id, result_cmp.permission_id, count(result_cmp.member_id)  FROM community_members cm,
+  (
+    SELECT permission_id, member_id  FROM community_member_permissions
+    ORDER BY permission_id 
+  )AS result_cmp
+  WHERE result_cmp.member_id=cm.user_id 
+  GROUP
+)AS result_cm
+GROUP BY p.name
+ORDER BY c.id DESC
